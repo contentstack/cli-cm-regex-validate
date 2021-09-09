1c0efe1e5cb657553a9a7ce78ca125d5a9b9e25d
Hardik Kundalia <hardik.kundalia@contentstack.com>
2021-09-09 17:38:43 +0530

    Added safe-regex Dependency & Util Function to check Safe Regex

---
+++ src/utils/safe-regex.ts
@@ -0,0 +1,61 @@
+const safe = require('safe-regex')
+
+const safeRegexCheck = (document: any, invalidRegex: any, tableData: any, type: string) => {
+  const beforeCount = invalidRegex.length
+  function checkSchemaFieldRegex(schema: any, currentPath = '', nested: boolean) {
+    let newPath = ''
+    function path(currentPath: string, uid: string) {
+      return currentPath.trim() === '' ? uid : [currentPath, uid].join('.')
+    }
+    schema.forEach((field: any) => {
+      newPath = path(currentPath, field.uid)
+      if (field.data_type === 'group' || field.data_type === 'global_field') {
+        if (field.schema) {
+          checkSchemaFieldRegex(field.schema, newPath, true)
+        }
+      }
+      if (field.data_type === 'blocks') {
+        field.blocks.forEach((block: any) => {
+          if (block.schema) {
+            newPath = path(newPath, block.uid)
+            checkSchemaFieldRegex(block.schema, newPath, true)
+            newPath = path(currentPath, field.uid)
+          }
+        })
+      }
+      if (field.format) {
+        const result = safe(field.format)
+        if (result) {
+          newPath = ''
+        } else {
+          const regexObject = {
+            'Module Type': type,
+            'Module Name': document.title,
+            'Module UID': document.uid,
+            'Field Name': field.display_name,
+            'Field UID': field.uid,
+            'Field Path': newPath,
+            'Invalid Regex': field.format,
+          }
+          invalidRegex.push(regexObject)
+          newPath = ''
+        }
+      } else {
+        newPath = ''
+      }
+    })
+    const currentCount = invalidRegex.length - beforeCount
+    if (currentCount > 0 && !nested) {
+      const tableDataObject = {
+        'Module Type': type,
+        'Module Name': document.title,
+        'Module UID': document.uid,
+        'Invalid Regex Count': currentCount,
+      }
+      tableData.push(tableDataObject)
+    }
+  }
+  checkSchemaFieldRegex(document.schema, '', false)
+}
+
+export default safeRegexCheck
