f5296a2ab44ac9c28aaeb2c5e0a40f0a1d0f5a3e
Hardik Kundalia <hardik.kundalia@contentstack.com>
2021-09-09 18:21:46 +0530

    Removed Commented Code

--- src/utils/get-management-token.ts
+++ src/utils/get-management-token.ts
@@ -3,15 +3,9 @@ const regexMessages = require('../../messages/index.json').validateRegex
 
 export default async function getManagementToken(alias: any) {
   const command = new Command()
-  // let tokenDetails: any
   try {
-    // const tokenDetails = command.getToken('Regex Check')
-    // tokenDetails = await command.getToken(alias)
-    // console.log(`${regexMessages.tokenDetails}${JSON.stringify(tokenDetails)}`)
-    // return tokenDetails
     return await command.getToken(alias)
   } catch (error) {
-    // console.error(regexMessages.errors.tokenNotFound)
     throw new Error(regexMessages.errors.tokenNotFound)
   }
 }
