b53617235bc8d24f4f6c3a3d9990920add10c450
Hardik Kundalia <hardik.kundalia@contentstack.com>
2021-09-12 02:34:53 +0530

    Moved Code into different Util Functions
    Used Connect Stack Util Function

--- src/commands/cm/stacks/validate-regex.ts
+++ src/commands/cm/stacks/validate-regex.ts
@@ -1,17 +1,9 @@
 import {Command, flags} from '@oclif/command'
-import {Command as CSCommand} from '@contentstack/cli-command'
-import {cli} from 'cli-ux'
-import * as contentstackSdk from '@contentstack/management'
-import * as Configstore from 'configstore'
-import * as jsonexport from 'jsonexport'
-import * as path from 'path'
-import * as fs from 'fs'
 import getAuthToken from '../../../utils/get-auth-token'
 import {inquireAlias, inquireModule} from '../../../utils/interactive'
 import getManagementToken from '../../../utils/get-management-token'
-import safeRegexCheck from '../../../utils/safe-regex'
+import connectStack from '../../../utils/connect-stack'
 const regexMessages = require('../../../../messages/index.json').validateRegex
-const config = new Configstore('contentstack_cli')
 
 export default class CmStacksValidateRegex extends Command {
   static description = 'This command is used to check for Invalid Regex in all the Content Types & Global Fields'
@@ -43,8 +35,6 @@ export default class CmStacksValidateRegex extends Command {
   async run() {
     const {flags} = this.parse(CmStacksValidateRegex)
 
-    const command = new CSCommand()
-
     let authToken
     try {
       authToken = await getAuthToken()
@@ -67,66 +57,10 @@ export default class CmStacksValidateRegex extends Command {
 
     await inquireModule(flags, regexMessages)
 
-    const startTime = Date.now()
-    cli.action.start(regexMessages.cliAction.connectStackStart, '', {stdout: true})
-
-    const client = contentstackSdk.client({
-      authtoken: config.get('authtoken'),
-    })
-
-    const stack = client.stack({api_key: tokenDetails.apiKey})
-    stack.fetch().then(async (stack: any) => {
-      cli.action.stop(regexMessages.cliAction.connectStackStop + (Date.now() - startTime) + ' ms')
-      const processTime = Date.now()
-      cli.action.start(regexMessages.cliAction.processStackStart, '', {stdout: true})
-      const query = {}
-      const invalidRegex: object[] = []
-      const tableData: object[] = []
-      if (flags.contentType) {
-        const contentTypes = stack.contentType().query(query).find()
-        await contentTypes.then((contentTypes: any) => {
-          contentTypes.items.forEach((contentType: any) => {
-            safeRegexCheck(contentType, invalidRegex, tableData, 'Content Type')
-          })
-        }).catch((error: Error) => {
-          this.log(regexMessages.errors.contentTypes, error)
-        })
-      }
-      if (flags.globalField) {
-        const globalFields = stack.globalField().query(query).find()
-        await globalFields.then((globalFields: any) => {
-          globalFields.items.forEach((globalField: any) => {
-            safeRegexCheck(globalField, invalidRegex, tableData, 'Global Field')
-          })
-        }).catch((error: Error) => {
-          this.log(regexMessages.errors.globalFields, error)
-        })
-      }
-      cli.action.stop(regexMessages.cliAction.processStackStop + (Date.now() - processTime) + ' ms')
-      const resultFile = 'results.csv'
-      let storagePath = path.resolve(__dirname, '../../../../', resultFile)
-      if (flags.filePath && fs.existsSync(flags.filePath)) {
-        storagePath = flags.filePath + resultFile
-      } else if (flags.filePath) {
-        fs.mkdirSync(flags.filePath, {recursive: true})
-        storagePath = flags.filePath + resultFile
-      }
-      if (invalidRegex.length > 0) {
-        jsonexport(invalidRegex, function (error: Error, csv: string) {
-          if (error) {
-            cli.error(regexMessages.errors.csvOutput)
-          }
-          fs.writeFileSync(storagePath, csv)
-        })
-        this.log(regexMessages.tableOutput)
-        console.table(tableData)
-        this.log(regexMessages.csvOutput, storagePath)
-        this.log(regexMessages.docsLink)
-      } else {
-        this.log(regexMessages.noInvalidRegex)
-      }
-    }).catch((error: Error) => {
-      this.log(regexMessages.errors.stacks, JSON.parse(error.message).errorMessage)
-    })
+    try {
+      await connectStack(flags, authToken, tokenDetails.apiKey)
+    } catch (error: any) {
+      this.error(regexMessages.errors.stack.fetch)
+    }
   }
 }
