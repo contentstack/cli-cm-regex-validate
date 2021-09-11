34e7e82d3e0445e781217daa989c00343ba5e4f1
Hardik Kundalia <hardik.kundalia@contentstack.com>
2021-09-12 03:41:26 +0530

    Added Unit Tests for Interactive Util Function

---
+++ test/utils/interactive.test.ts
@@ -0,0 +1,78 @@
+const inquirer = require('inquirer')
+const {inquireAlias, inquireModule, validateAlias, validateModule} = require('../../src/utils/interactive.ts')
+const regexMessages = require('../../messages/index.json').validateRegex
+
+describe('Interactive', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  test('Alias Token Flag is Set', async () => {
+    const flags = {alias: 'Test Token'}
+    const response = await inquireAlias(flags)
+    expect(response).toBeUndefined()
+  })
+
+  test('Alias Token is not Entered', async () => {
+    const alias = ''
+    const response = await validateAlias(alias)
+    expect(response).toBe(regexMessages.required)
+  })
+
+  test('Alias Token is Entered', async () => {
+    const alias = 'Test Token'
+    const flags = {}
+    const response = await validateAlias(alias)
+    expect(response).toBe(true)
+    jest.spyOn(inquirer, 'prompt').mockImplementation(() => {
+      return {alias: alias}
+    })
+    await inquireAlias(flags)
+  })
+
+  test('Module Flags are Set', async () => {
+    async function testModuleFlags(flags: object) {
+      const response = await inquireModule(flags)
+      expect(response).toBeUndefined()
+    }
+    testModuleFlags({contentType: true})
+    testModuleFlags({globalField: true})
+    testModuleFlags({contentType: true, globalField: true})
+  })
+
+  test('Module is not Selected', async () => {
+    const choice: string[] = []
+    const response = await validateModule(choice)
+    expect(response).toBe(regexMessages.selectOne)
+  })
+
+  test('Content Type Module is Selected', async () => {
+    const choice: string[] = ['contentType']
+    const response = await validateModule(choice)
+    expect(response).toBe(true)
+    jest.spyOn(inquirer, 'prompt').mockImplementation(() => {
+      return {choice: choice}
+    })
+    await inquireModule(choice)
+  })
+
+  test('Global Field Module is Selected', async () => {
+    const choice: string[] = ['globalField']
+    const response = await validateModule(choice)
+    expect(response).toBe(true)
+    jest.spyOn(inquirer, 'prompt').mockImplementation(() => {
+      return {choice: choice}
+    })
+    await inquireModule(choice)
+  })
+
+  test('Both Modules are Selected', async () => {
+    const choice: string[] = ['contentType', 'globalField']
+    const response = await validateModule(choice)
+    expect(response).toBe(true)
+    jest.spyOn(inquirer, 'prompt').mockImplementation(() => {
+      return {choice: choice}
+    })
+    await inquireModule(choice)
+  })
+})
