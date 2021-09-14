f6538970c77fa681d9a919d041473cc93f01ff7a
Hardik Kundalia <hardik.kundalia@contentstack.com>
2021-09-14 15:31:21 +0530

    Moved Command Help Text into UI Messages Json File

--- src/commands/cm/stacks/validate-regex.ts
+++ src/commands/cm/stacks/validate-regex.ts
@@ -6,30 +6,26 @@ import connectStack from '../../../utils/connect-stack'
 const regexMessages = require('../../../../messages/index.json').validateRegex
 
 export default class CmStacksValidateRegex extends Command {
-  static description = 'This command is used to find all the invalid regexes present in the content types and global fields of your stack.'
+  static description = regexMessages.command.description
 
   static flags = {
-    help: flags.help({char: 'h', description: 'To show details for the CLI command'}),
-    alias: flags.string({char: 'a', description: 'Alias (name) assigned to the management token'}),
-    contentType: flags.boolean({char: 'c', description: 'To find invalid regexes within the content types'}),
-    globalField: flags.boolean({char: 'g', description: 'To find invalid regexes within the global fields'}),
-    filePath: flags.string({char: 'f', description: 'The path or the location in your file system where the CSV output file should be stored, e.g., -f "C:\\Users\\Name\\Desktop\\cli\\csv"'}),
+    help: flags.help({char: 'h', description: regexMessages.command.help}),
+    alias: flags.string({char: 'a', description: regexMessages.command.alias}),
+    contentType: flags.boolean({char: 'c', description: regexMessages.command.contentTypes}),
+    globalField: flags.boolean({char: 'g', description: regexMessages.command.globalFields}),
+    filePath: flags.string({char: 'f', description: regexMessages.command.filePath}),
   }
 
-  static usage = '$ csdx cm:stacks:validate-regex --alias=[ALIAS_NAME] --contentType --globalField --filePath=[FILE_PATH]'
+  static usage = 'cm:stacks:validate-regex --alias=[ALIAS_NAME] --contentType --globalField --filePath=[FILE_PATH]'
 
   static examples = [
     '$ csdx cm:stacks:validate-regex',
     '$ csdx cm:stacks:validate-regex --alias=\'Alias Name\' -a \'Alias Name\'',
     '$ csdx cm:stacks:validate-regex --contentType -c',
     '$ csdx cm:stacks:validate-regex --globalField -g',
-    '$ csdx cm:stacks:validate-regex --filePath=\'path/to/the/directory/\' -f \'path/to/the/directory/\'',
-    '$ csdx cm:stacks:validate-regex --alias=\'Alias Name\' --contentType --globalField',
-    '$ csdx cm:stacks:validate-regex --alias=\'Alias Name\' --contentType --globalField --filePath=\'path/to/the/directory/\'',
+    '$ csdx cm:stacks:validate-regex --filePath=\'path/to/the/directory/\' -f \'path/to/the/directory\'',
     '$ csdx cm:stacks:validate-regex -a \'Alias Name\' -c -g',
-    '$ csdx cm:stacks:validate-regex -a \'Alias Name\' -c -g -f \'path/to/the/directory/\'',
-    '$ csdx cm:stacks:validate-regex -cga \'Alias Name\'',
-    '$ csdx cm:stacks:validate-regex -cga \'Alias Name\' -f \'path/to/the/directory/\'',
+    '$ csdx cm:stacks:validate-regex -a \'Alias Name\' -c -g -f \'path/to/the/directory\'',
   ]
 
   async run() {
